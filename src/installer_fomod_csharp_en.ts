319c1bb701e69703ad6a896dc083e4a1325ad5b0
Mikaël Capelle <capelle.mikael@gmail.com>
2020-05-18 22:46:06 +0200

    Update translation file.

--- src/installer_fomod_csharp_en.ts
+++ src/installer_fomod_csharp_en.ts
@@ -4,30 +4,40 @@
 <context>
     <name>FomodCSharpPredialog</name>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="14"/>
         <source>FOMOD C# Installer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="22"/>
         <source>Name</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="40"/>
+        <location filename="installer_fomod_csharp_predialog.ui" line="43"/>
+        <location filename="installer_fomod_csharp_predialog.ui" line="53"/>
+        <location filename="installer_fomod_csharp_predialog.ui" line="56"/>
         <source>Opens a Dialog that allows custom modifications.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="46"/>
         <source>Manual</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="59"/>
         <source>Use NCC</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="82"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="installer_fomod_csharp_predialog.ui" line="89"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
@@ -35,6 +45,7 @@
 <context>
     <name>FomodInfoReader</name>
     <message>
+        <location filename="xml_info_reader.h" line="106"/>
         <source>Failed to parse %1. See console for details.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -42,12 +53,14 @@
 <context>
     <name>QObject</name>
     <message>
-        <source>The following settings should be updated:
-%1</source>
+        <location filename="base_script.cpp" line="89"/>
+        <source>Settings should be updated</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <source>Settings should be updated</source>
+        <location filename="base_script.cpp" line="90"/>
+        <source>The following settings should be updated:
+%1</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
